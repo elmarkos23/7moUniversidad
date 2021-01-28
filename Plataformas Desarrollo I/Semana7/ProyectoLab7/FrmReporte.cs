b4f80d63dc75db4649dfaf125b7c303830d49bd6
Markos Ayala <elmarkos23@gmail.com>
2021-01-28 00:18:00 -0500

    ok

---
+++ Plataformas Desarrollo I/Semana7/ProyectoLab7/FrmReporte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoLab7
+{
+  public partial class FrmReporte : Form
+  {
+    public FrmReporte()
+    {
+      InitializeComponent();
+    }
+
+    private void FrmReporte_Load(object sender, EventArgs e)
+    {
+      // TODO: esta línea de código carga datos en la tabla 'DataSet1.Usuarios' Puede moverla o quitarla según sea necesario.
+      this.UsuariosTableAdapter.Fill(this.DataSet1.Usuarios);
+
+      this.reportViewer1.RefreshReport();
+    }
+  }
+}
